61d8ae3b6b90ff89f8b5c28500c69a63855a7c6d
AmbulantRex <21176662+AmbulantRex@users.noreply.github.com>
2021-08-14 08:36:41 -0600

    Converted empty array initialization to Array.Empty<T>
    # Conflicts:
    #	sources/core/Xenko.Core/Collections/ArrayHelper.cs
    #	sources/engine/Stride.VirtualReality/OculusOVR/OculusOvrHmd.cs
    #	sources/engine/Stride.VirtualReality/WindowsMixedReality/WindowsMixedRealityHmd.cs

--- sources/core/Xenko.Core/Collections/FastList.cs
+++ sources/core/Xenko.Core/Collections/FastList.cs
@@ -33,7 +33,7 @@ namespace Xenko.Core.Collections
 
         public FastList()
         {
-            Items = ArrayHelper<T>.Empty;
+            Items = Array.Empty<T>();
         }
 
         public FastList([NotNull] IEnumerable<T> collection)
@@ -83,7 +83,7 @@ namespace Xenko.Core.Collections
                     }
                     else
                     {
-                        Items = ArrayHelper<T>.Empty;
+                        Items = Array.Empty<T>();
                     }
                 }
             }
--- sources/engine/Xenko.Particles/Sorters/ArrayPool.cs
+++ sources/engine/Xenko.Particles/Sorters/ArrayPool.cs
@@ -2,9 +2,6 @@
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Xenko.Particles.Sorters
 {
@@ -12,7 +9,7 @@ namespace Xenko.Particles.Sorters
     {
         private readonly Dictionary<int, Stack<T[]>> _pool = new Dictionary<int, Stack<T[]>>();
 
-        public readonly T[] Empty = new T[0];
+        public static readonly T[] Empty = Array.Empty<T>();
 
         public virtual void Clear()
         {
