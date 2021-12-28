f8d2683e2b566f23b79d6ab19eb3999ce7f4b555
Phr00t <phr00t@gmail.com>
2021-12-28 16:43:48 -0500

    Tools: fix UV maps with importing models

--- sources/tools/Xenko.Importer.Assimp/Utils.cs
+++ sources/tools/Xenko.Importer.Assimp/Utils.cs
@@ -1,4 +1,4 @@
-﻿using Xenko.Animations;
+using Xenko.Animations;
 using Xenko.Core.Mathematics;
 using System;
 using System.Collections.Generic;
@@ -35,11 +35,15 @@ namespace Xenko.Importer.Assimp
         public static unsafe uint GetNumUVChannels(Silk.NET.Assimp.Mesh* mesh)
         {
             var n = 0;
-            var AI_MAX_NUMBER_OF_TEXTURECOORDS = 8;
-            while (n < AI_MAX_NUMBER_OF_TEXTURECOORDS && mesh->MColors[n] != null)
-            {
-                ++n;
-            }
+
+            if (mesh->MTextureCoords.Element0 != null) n++;
+            if (mesh->MTextureCoords.Element1 != null) n++;
+            if (mesh->MTextureCoords.Element2 != null) n++;
+            if (mesh->MTextureCoords.Element3 != null) n++;
+            if (mesh->MTextureCoords.Element4 != null) n++;
+            if (mesh->MTextureCoords.Element5 != null) n++;
+            if (mesh->MTextureCoords.Element6 != null) n++;
+            if (mesh->MTextureCoords.Element7 != null) n++;
 
             return (uint)n;
         }
