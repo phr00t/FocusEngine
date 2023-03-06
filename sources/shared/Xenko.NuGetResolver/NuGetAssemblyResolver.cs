b70f1b85dffdec9437f66767cef97cc9b48fb799
Phr00t <phr00t@gmail.com>
2023-03-06 13:33:33 -0500

    NuGet Resolver: fix recent issues and make it more reliable

--- sources/shared/Xenko.NuGetResolver/NuGetAssemblyResolver.cs
+++ sources/shared/Xenko.NuGetResolver/NuGetAssemblyResolver.cs
@@ -6,25 +6,106 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Versioning;
 
+/*
+ *  System.Private.CoreLib is only being found in (x86) directories?
+ *    - need to add debug lines for every library found.. see whats happening this this library
+ *    - library fails because it can't find x64 version
+ *    - sort things x64 to the top?
+ */
+
 namespace Xenko.Core.Assets
 {
     class NuGetAssemblyResolver
     {
+        const bool DEBUG_RESOLVE = false;
+
         static bool assembliesResolved;
         static object assembliesLock = new object();
-        static Dictionary<string, string> assemblyLocationWithVersion = new Dictionary<string, string>();
-        static Dictionary<string, string> xenkoAssemblies = new Dictionary<string, string>();
-        static Dictionary<string, List<string>> allAssemblies = new Dictionary<string, List<string>>();
+        static Dictionary<string, List<LibraryInfo>> allAssemblies = new Dictionary<string, List<LibraryInfo>>();
 
         internal static void DisableAssemblyResolve()
         {
             assembliesResolved = true;
+        }
+
+        internal class LibraryInfo : IComparable<LibraryInfo>
+        {
+            public string Name;
+            public string Version;
+            public string FullPath;
+            public int LibraryRating;
+            public int[] versionInfo;
+
+            public LibraryInfo(string vers)
+            {
+                Version = vers;
+                string[] sv = vers.Split('.');
+                versionInfo = new int[sv.Length];
+                try
+                {
+                    for (int i = 0; i < sv.Length; i++)
+                        versionInfo[i] = int.Parse(sv[i]);
+                }
+                catch (Exception e)
+                {
+                    if (DEBUG_RESOLVE)
+                        System.Console.Error.WriteLine("Couldn't parse version: " + vers);
+                }
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj is LibraryInfo li)
+                    return li.CompareTo(this);
+
+                return 0;
+            }
+
+            public int CompareTo(LibraryInfo other)
+            {
+                if (LibraryRating > other.LibraryRating) return -1;
+                if (LibraryRating < other.LibraryRating) return 1;
+                for (int i = 0; i < versionInfo.Length && i < other.versionInfo.Length; i++)
+                {
+                    if (versionInfo[i] > other.versionInfo[i]) return -1;
+                    if (versionInfo[i] < other.versionInfo[i]) return 1;
+                }
+                return 0;
+            }
+
+            public static int GetLibraryRating(string path)
+            {
+                if (path.Contains("net48"))
+                    return 10;
+                if (path.Contains("net47"))
+                    return 9;
+                if (path.Contains("net46"))
+                    return 8;
+                if (path.Contains("net4"))
+                    return 7;
+                if (path.Contains("net3"))
+                    return 6;
+                if (path.Contains("net2"))
+                    return 5;
+                if (path.Contains("net1"))
+                    return 4;
+                if (path.Contains("netstandard2.0"))
+                    return 2;
+                if (path.Contains("netstandard"))
+                    return 1;
+                if (path.Contains("net6.0"))
+                    return 0;
+                if (path.Contains("net5.0"))
+                    return -1;
+                return -2;
+            }
         }
 
         [ModuleInitializer(-100000)]
@@ -77,61 +158,98 @@ namespace Xenko.Core.Assets
                     if (!assembliesResolved)
                     {
                         assembliesResolved = true;
+
                         var settings = NuGet.Configuration.Settings.LoadDefaultSettings(null);
                         var packageSourceProvider = new PackageSourceProvider(settings);
-                        var installPath = SettingsUtility.GetGlobalPackagesFolder(settings);
-                        var nugetAssemblies = new List<string>(Directory.GetFiles(installPath, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
-                        nugetAssemblies.AddRange(Directory.GetFiles(installPath, "*.exe", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
 
+                        var installPath = SettingsUtility.GetGlobalPackagesFolder(settings);
                         var extraPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\dotnet\\shared\\Microsoft.NETCore.App";
                         var extraPath86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "\\dotnet\\shared\\Microsoft.NETCore.App";
-                        var extraAssemblies = new List<string>(Directory.GetFiles(extraPath, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
-                        extraAssemblies.AddRange(Directory.GetFiles(extraPath86, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
+
+                        var nugetAssemblies = new List<string>(Directory.GetFiles(installPath, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
+                        nugetAssemblies.AddRange(Directory.GetFiles(installPath, "*.exe", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
+                        nugetAssemblies.AddRange(Directory.GetFiles(extraPath, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
+                        nugetAssemblies.AddRange(Directory.GetFiles(extraPath86, "*.dll", SearchOption.AllDirectories).OrderByDescending(f => new FileInfo(f).CreationTime));
 
                         if (xenkoLibFolder != null)
-                        {
-                            foreach (string f in Directory.GetFiles(xenkoLibFolder, "*.dll", SearchOption.AllDirectories))
-                                xenkoAssemblies[Path.GetFileNameWithoutExtension(f)] = f;
-                        }
+                            nugetAssemblies.AddRange(Directory.GetFiles(xenkoLibFolder, "*.dll", SearchOption.AllDirectories));
 
                         // only include stuff we want
                         for (int i=0; i<nugetAssemblies.Count; i++)
                         {
                             var assembly = nugetAssemblies[i];
 
-                            if (assembly.Contains("linux") || assembly.Contains("osx"))
+                            if (assembly.Contains("linux") || assembly.Contains("osx") || 
+                                assembly.Contains("\\runtimes\\") && !assembly.Contains("xenko") ||
+                                assembly.Contains("\\ref\\"))
                                 continue;
 
-                            var dir = assembly.Split('\\');
-                            var version = dir[Array.IndexOf(dir, "packages") + 2].Split('.', '-');
-                            var assmName = Path.GetFileNameWithoutExtension(assembly);
-                            var versionProcessed = version[0] + "." + version[1] + "." + version[2];
-                            if (version.Length > 3 && int.TryParse(version[3], out int res))
-                                versionProcessed += "." + res;
-                            else
-                                versionProcessed += ".0";
-                            var key = assmName + "-" + versionProcessed;
+                            LibraryInfo li;
 
-                            if (assemblyLocationWithVersion.ContainsKey(key) == false)
-                                assemblyLocationWithVersion[key] = assembly;
-
-                            if (allAssemblies.TryGetValue(assmName, out var list))
-                                list.Add(assembly);
-                            else
-                                allAssemblies[assmName] = new List<string>() { assembly };
-                        }
-
-                        // get the rest of assembly versions
-                        for (int i=0; i<extraAssemblies.Count; i++)
-                        {
-                            var assembly = extraAssemblies[i];
                             var assmName = Path.GetFileNameWithoutExtension(assembly);
 
-                            if (allAssemblies.TryGetValue(assmName, out var list))
-                                list.Add(assembly);
+                            if (assembly.Contains("Xenko.GameStudio"))
+                            {
+                                // xenko
+                                li = new LibraryInfo("9.9.1.0")
+                                {
+                                    FullPath = assembly,
+                                    Name = assmName,
+                                    LibraryRating = 15                                     
+                                };
+                            } 
                             else
-                                allAssemblies[assmName] = new List<string>() { assembly };
+                            {
+                                var dir = assembly.Split('\\');
+                                if (assembly.Contains("dotnet\\shared"))
+                                {
+                                    // dotnetshared
+                                    var version = dir[dir.Length - 2] + ".0";
+                                    li = new LibraryInfo(version)
+                                    {
+                                        FullPath = assembly,
+                                        Name = assmName,
+                                        LibraryRating = LibraryInfo.GetLibraryRating(assembly)
+                                    };
+                                } 
+                                else
+                                {
+                                    // nuget stuff
+                                    var version = dir[Array.IndexOf(dir, "packages") + 2].Split('.', '-');
+                                    var versionProcessed = version[0] + "." + version[1] + "." + version[2];
+                                    if (version.Length > 3 && int.TryParse(version[3], out int res))
+                                        versionProcessed += "." + res;
+                                    else
+                                        versionProcessed += ".0";
+
+                                    li = new LibraryInfo(versionProcessed)
+                                    {
+                                        Name = assmName,
+                                        FullPath = assembly,
+                                        LibraryRating = LibraryInfo.GetLibraryRating(assembly)
+                                    };
+                                }
+                            }
+
+                            if (allAssemblies.TryGetValue(assmName, out var sorted))
+                            {
+                                if (DEBUG_RESOLVE)
+                                    System.Console.Error.WriteLine("[" + assmName + "] added with " + li.FullPath);
+
+                                sorted.Add(li);
+                            }
+                            else
+                            {
+                                if (DEBUG_RESOLVE)
+                                    System.Console.Error.WriteLine("[" + assmName + "] Started with " + li.FullPath);
+
+                                allAssemblies[assmName] = new List<LibraryInfo>() { li };
+                            }
                         }
+
+                        // sort all the lists
+                        foreach (var l in allAssemblies.Values)
+                            l.Sort();
                     }
 
                     if (assembliesResolved)
@@ -140,57 +258,55 @@ namespace Xenko.Core.Assets
                         if (aname.Name.StartsWith("Microsoft.Build") && aname.Name != "Microsoft.Build.Locator")
                             return null;
 
-                        string assemblyPath;
-
-                        // if we are xenko, use the xenko built stuff first
-                        if (xenkoAssemblies.TryGetValue(aname.Name, out assemblyPath))
+                        if (allAssemblies.TryGetValue(aname.Name, out var libs))
                         {
-                            try
+                            if (DEBUG_RESOLVE)
+                                System.Console.Error.WriteLine("[" + eventArgs.Name + "] Found with " + libs.Count + " libraries.");
+
+                            List<string> versionsToTry = new List<string>();
+
+                            if (aname.Name == "System.Numerics.Vectors")
                             {
-                                return Assembly.LoadFrom(assemblyPath);
+                                versionsToTry.Add("4.4.0.0");
+                                versionsToTry.Add("4.5.0.0");
                             }
-                            catch (Exception e) { } // try another key
-                        }
+                            else versionsToTry.Add(aname.Version.ToString());
 
-                        List<string> keysToTry = new List<string>();
-
-                        // numerics has terrible version naming
-                        if (aname.Name == "System.Numerics.Vectors")
-                        {
-                            keysToTry.Add(aname.Name + "-4.4.0.0");
-                            keysToTry.Add(aname.Name + "-4.5.0.0");
-                        } else keysToTry.Add(aname.Name + "-" + aname.Version.ToString());
-
-                        for (int i=0; i<keysToTry.Count; i++)
-                        {
-                            var key = keysToTry[i];
-
-                            if (assemblyLocationWithVersion.TryGetValue(key, out assemblyPath))
+                            for (int i=0; i<versionsToTry.Count; i++)
                             {
-                                try
+                                for (int j=0; j<libs.Count; j++)
                                 {
-                                    return Assembly.LoadFrom(assemblyPath);
-                                } 
-                                catch (Exception e) { } // try another key
-                            }
-                        }                       
+                                    var libToTry = libs[j];
+                                    if (libToTry.Version != versionsToTry[i]) continue;
+                                    try
+                                    {
+                                        if (DEBUG_RESOLVE)
+                                            System.Console.Error.WriteLine("[" + eventArgs.Name + "] Trying version specific load: " + libToTry.FullPath);
 
-                        // fallback searching all assemblies
-                        if (allAssemblies.TryGetValue(aname.Name, out var list))
-                        {
-                            for(int i=0; i<list.Count; i++)
-                            {
-                                try
-                                {
-                                    if ((aname.Version.Major == 6 || list[i].Contains("\\6.")) && list[i].Contains("runtimes")) continue; // version 6 stuff don't use runtime libraries
-                                    if (list[i].Contains("\\ref\\") || list[i].Contains("netcoreapp3.1")) continue; // don't use these things in backups
-                                    return Assembly.LoadFrom(list[i]);
+                                        return Assembly.LoadFrom(libToTry.FullPath);
+                                    } catch(Exception) { }
                                 }
-                                catch (Exception e) { }
+                            }
+
+                            // versions didn't work... try them in order
+                            for (int i=0; i<libs.Count; i++)
+                            {
+                                try
+                                {
+                                    if (DEBUG_RESOLVE)
+                                        System.Console.Error.WriteLine("[" + eventArgs.Name + "] Trying version priority load: " + libs[i].FullPath);
+
+                                    return Assembly.LoadFrom(libs[i].FullPath);
+                                }
+                                catch (Exception) { }
                             }
                         }
                     }
                 }
+
+                if (DEBUG_RESOLVE)
+                    System.Console.Error.WriteLine("[" + eventArgs.Name + "] No library found!");
+
                 return null;
             };
         }
